83581908bf5aae8eac7a5cdcddddee83576df97e
Yury Shashurin <shashurin.edu@mail.ru>
2021-02-24 23:44:59 +0300

    first commit

---
+++ gallow.py
@@ -0,0 +1,102 @@
+import random
+WORDS = ['skillfactory', 'testing', 'blackbox', 'pytest', 'unittest', 'coverage']
+def random_word(WORDS):
+    return random.choice(WORDS)
+
+
+class GallowGame():
+
+    def __init__(self):
+        self.name = input('Как к тебе можно обращаться? ')
+        self.hidden_word = random_word(WORDS)
+        self.MAX_WRONG_ATTEMPTS = 4
+        self.current_wrong_attempts = 0
+        self.user_letters = []
+        self.guessed = []
+
+    def valid_user_input(self, user_letter):
+        if len(user_letter) > 1 or not user_letter.isalpha():
+            print('Неправильный ввод. Введите одну букву')
+            self.current_wrong_attempts += 1
+            print('{} из {} попыток использованы. Будьте внимательней'.format(self.current_wrong_attempts, self.MAX_WRONG_ATTEMPTS))
+        elif user_letter in self.user_letters:
+            print('Буква уже была использована')
+            self.current_wrong_attempts += 1
+            print('{} из {} попыток использованы. Хорошенько подумайте'.format(self.current_wrong_attempts, self.MAX_WRONG_ATTEMPTS))
+        else:
+            return True
+
+    def show_result(self, user_letter):
+        self.user_letters.append(user_letter)
+        word = []
+        for letter in self.hidden_word:
+            if letter in self.user_letters:
+                word.append(letter)
+            else:
+                word.append('_')
+
+        show_result = ''
+        for l in word:
+            show_result += l
+
+        self.guessed = show_result
+        print('Текущий статус -',self.guessed)
+        return self.guessed
+
+    def check_letter(self,user_letter):
+        if user_letter.lower() in self.hidden_word:
+            print('Вы угадали букву')
+            self.show_result(user_letter)
+            return True
+        else:
+            print('Увы, вы ошиблись')
+            self.current_wrong_attempts += 1
+            print('Допущена {} ошибка из {} возможных'.format(self.current_wrong_attempts, self.MAX_WRONG_ATTEMPTS))
+            self.show_result(user_letter)
+            return False
+
+    def check_attemps(self):
+        if self.current_wrong_attempts == self.MAX_WRONG_ATTEMPTS:
+            print('=======================================')
+            print("GameOver. В следующий раз тебе повезет, {}!".format(self.name).upper())
+            print('Загаданное слово -',self.hidden_word)
+            print('=======================================')
+            return True
+        elif self.hidden_word == self.guessed:
+            print('=======================================')
+            print('Поздравляю, {}, это победа! А ты силен!'.format(self.name).upper())
+            print('Загаданное слово -', self.hidden_word)
+            print('=======================================')
+            return True
+        else:
+            print('Продолжаем угадывать')
+            print('*********')
+            return False
+
+
+
+
+
+
+def main():
+    game = GallowGame()
+    print("Привет, {}. Давай поиграем!".format(game.name))
+    print("У тебя есть 4 попытки угадать загаданное слово. Поееехаллии!")
+    print("Загадано слово из",len(game.hidden_word), "букв.")
+    while True:
+        user_letter = input("Введите букву: ")
+        valid = game.valid_user_input(user_letter)
+        if not valid:
+            result = game.check_attemps()
+            if result:
+                break
+            else:
+                continue
+        game.check_letter(user_letter)
+        result = game.check_attemps()
+        if result:
+            break
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_cases.py
@@ -0,0 +1,79 @@
+import pytest
+from gallow import WORDS, GallowGame, random_word
+
+WORD = 'blackbox'
+
+def test_random_word():
+    game_test  = GallowGame()
+    assert game_test.hidden_word in WORDS
+
+@pytest.mark.parametrize("input_letter", ['a', 'b', 'w', 'f'])
+def test_valid_input_right_case(input_letter):
+    game_test = GallowGame()
+    result_valid = game_test.valid_user_input(input_letter)
+    assert result_valid
+
+@pytest.mark.parametrize("input_letter", ['aa', '1', 'b1', '1a'])
+def test_valid_input_wrong_case(input_letter):
+    game_test = GallowGame()
+    result_valid = game_test.valid_user_input(input_letter)
+    assert not result_valid
+
+def test_check_create_game():
+    name = 'Yuriy'
+    game_test = GallowGame()
+    game_test.name = name
+    assert game_test.name == name
+    assert game_test.MAX_WRONG_ATTEMPTS == 4
+    assert game_test.current_wrong_attempts == 0
+    assert game_test.user_letters == {}
+    assert game_test.guessed == {}
+    assert game_test.hidden_word in WORDS
+
+@pytest.mark.parametrize("input_letter", ['b', 'c', 'k', 'x'])
+def test_show_result_if_in_hidden(input_letter):
+    game_test = GallowGame()
+    game_test.hidden_word = WORD
+    game_test.show_result(input_letter)
+    assert input_letter in game_test.guessed
+
+@pytest.mark.parametrize("input_letter", ['f', 'w', 'z', 'y'])
+def test_show_result_if_not_in_hidden(input_letter):
+    game_test = GallowGame()
+    game_test.hidden_word = WORD
+    game_test.show_result(input_letter)
+    assert input_letter not in game_test.guessed
+
+@pytest.mark.parametrize("input_letter", ['b', 'c', 'k', 'x'])
+def test_check_result_if_in_hidden(input_letter):
+    game_test = GallowGame()
+    game_test.hidden_word = WORD
+    assert game_test.check_letter(input_letter)
+
+
+@pytest.mark.parametrize("input_letter", ['f', 'w', 'z', 'y'])
+def test_check_result_if_not_in_hidden(input_letter):
+    game_test = GallowGame()
+    game_test.hidden_word = WORD
+    assert not game_test.check_letter(input_letter)
+
+def check_count_wrong_attempts():
+    game_test = GallowGame()
+    assert game_test.current_wrong_attempts + 1 == 1
+
+@pytest.mark.parametrize("count", [0, 1, 2, 3,])
+def check_continue_game(count):
+    game_test = GallowGame()
+    game_test.current_wrong_attempts = count
+    assert not game_test.check_attemps()
+
+def check_win_game():
+    game_test = GallowGame()
+    game_test.hidden_word = WORD
+    game_test.guessed = WORD
+    assert game_test.hidden_word == game_test.guessed
+
+def check_fail_game():
+    game_test = GallowGame()
+    game_test.current_wrong_attempts = 4
+    assert game_test.check_attemps()
